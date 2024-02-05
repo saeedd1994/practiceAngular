8cbca68de18efc5630799e8fc308981f539e19ae
saeed.da1994 <saeed.da1994@gmail.com>
2024-02-05 22:43:34 +0330

    -practice rxjs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {AppComponent} from "./app.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: "full"
+  }
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, Observer} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    const interval$ = new Observable((observer: Observer<number>) => {
+      observer.next(1)
+      observer.next(2)
+      observer.next(3)
+      // observer.error(new Error('error in app!'))
+      observer.complete
+    });
+
+    interval$.subscribe(
+      (value: number)=>{
+        console.log(value)
+      }
+    );
+  }
 
 }
