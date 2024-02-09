3c0ff02ca97251b38583e0c9b480ad7a16db6be9
saeed.da1994 <saeed.da1994@gmail.com>
2024-02-09 11:24:14 +0330

    -add fetch into observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbAlert, NgbNav, NgbNavContent, NgbNavItem, NgbNavLinkButton, NgbNavOutlet} from "@ng-bootstrap/ng-bootstrap";
+import {Observable} from "rxjs";
+import {ITodo} from "../interface/itodo";
 
 @Component({
   selector: 'app-about',
@@ -18,23 +20,31 @@ import {NgbAlert, NgbNav, NgbNavContent, NgbNavItem, NgbNavLinkButton, NgbNavOut
 export class AboutComponent implements OnInit {
   active = 1;
   url: string = 'https://jsonplaceholder.typicode.com/todos';
+  http$?: Observable<ITodo[]>
 
   ngOnInit() {
 
+    this.http$ = new Observable<ITodo[]>((observer) => {
 
-    fetch(this.url)
-      .then((res) => {
-        return res.json()
-      })
-      .then((todos) => {
-        console.log(todos)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
 
+      fetch(this.url)
+        .then((res) => {
+          return res.json()
+        })
+        .then((todos) => {
+          observer.next(todos);
+          observer.complete();
+        })
+        .catch((err) => {
+          observer.error(err)
+        })
+
+
+    })
 
   }
+
+
 
 
 }
