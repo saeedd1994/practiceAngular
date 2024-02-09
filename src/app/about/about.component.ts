d65a81b07ca3280d3583786ddf018035915a3b7d
saeed.da1994 <saeed.da1994@gmail.com>
2024-02-09 11:01:04 +0330

    -add todo model -add tab section from ng bootstrap

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgbAlert, NgbNav, NgbNavContent, NgbNavItem, NgbNavLinkButton, NgbNavOutlet} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbNavItem,
+    NgbNavLinkButton,
+    NgbNav,
+    NgbNavContent,
+    NgbNavOutlet,
+    NgbAlert
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  active = 1;
 }
